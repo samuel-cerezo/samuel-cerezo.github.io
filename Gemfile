cbc46b40071a4bbc635f6b2d5d33c03e9c389867
Samuel Cerezo <samuel@gmail.com>
2025-07-09 18:44:41 +0200

    update

--- Gemfile
+++ Gemfile
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 source "https://rubygems.org"
 
-gem "jekyll"
-gem "minima"
-gem "csv"
-gem "base64"
+# gem "rails"
 
 gem "webrick", "~> 1.8"
+
+gem "jekyll", "~> 4.3"
+
+gem "minima", "~> 2.5"
--- js/index.js
+++ js/index.js
@@ -1,29 +1,11 @@
+
 $(document).ready(function() {
     $('.publication-mousecell').mouseover(function() {
-        var mouseoverVideo = $(this).find('video');
-        var mouseoverImage = $(this).find('.mouseover-image');
-        var defaultImage = $(this).find('img').not('.mouseover-image');
-
-        if (mouseoverVideo.length > 0) {
-            mouseoverVideo.css('display', 'inline-block');
-            defaultImage.css('display', 'none');
-        } else if (mouseoverImage.length > 0) {
-            mouseoverImage.css('display', 'inline-block');
-            defaultImage.css('display', 'none');
-        }
+        $(this).find('video').css('display', 'inline-block');
+        $(this).find('img').css('display', 'none');
     });
-
     $('.publication-mousecell').mouseout(function() {
-        var mouseoverVideo = $(this).find('video');
-        var mouseoverImage = $(this).find('.mouseover-image');
-        var defaultImage = $(this).find('img').not('.mouseover-image');
-
-        if (mouseoverVideo.length > 0) {
-            mouseoverVideo.css('display', 'none');
-            defaultImage.css('display', 'inline-block');
-        } else if (mouseoverImage.length > 0) {
-            mouseoverImage.css('display', 'none');
-            defaultImage.css('display', 'inline-block');
-        }
+        $(this).find('video').css('display', 'none');
+        $(this).find('img').css('display', 'inline-block');
     });
-});
+})
